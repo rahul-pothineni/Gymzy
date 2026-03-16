cf4c87e14d8b32d892d1d257d63b9ab44dd1eaad
rahul-pothineni <rahulpot07@gmail.com>
2026-03-15 20:07:49 -0400

    feat: Finished the most basic features of the navbar.

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,7 +2,7 @@ import { Anchor } from "lucide-react";
 import {Link} from "react-router-dom";
 import { Button } from "../ui/Button";
 export default function Navbar(){
-    const user = false;
+    const user = false; //TO;DO will be replaced later on with if the user is authenticated or not. 
     return (
         <header className = "fixed top-0 left-0 right-0 z-50 border-b border-[var(--color-border)] bg-[var(--color-background)]/80 backdrop-blur-md"> 
             <div className = "max-w-6xl mx-auto px-6 h-16 flex items-center justify-between">
@@ -14,6 +14,16 @@ export default function Navbar(){
                 </Link>
 
                 <nav>
+                    {user?  //check if user is authenticated, if they are give profile icon, if not give sign-in+up options
+                    
+                    <>
+                        <Link to="/profile">
+                            <Button variant = "ghost" size = "sm">My Plan</Button>
+                        </Link>
+                    </> 
+                    
+                    : 
+                    
                     <>
                         <Link to="/auth/sign-in">
                             <Button variant = "ghost" size = "sm">Sign In</Button>
@@ -22,6 +32,8 @@ export default function Navbar(){
                             <Button size = "sm">Sign Up</Button>
                         </Link>
                     </>
+                    }
+                    
                 </nav>
             </div>
         </header>
