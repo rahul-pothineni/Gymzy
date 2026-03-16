f97f4ddab77fa17be4a5fdc498ee670e13cfa7b6
rahul-pothineni <rahulpot07@gmail.com>
2026-03-15 20:00:10 -0400

    Initial Commit: Basic React Routing, Simple NavBar component, copy + past button component.

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Onboarding from "./pages/Onboarding";
+import Profile from "./pages/Profile";
+import Auth from "./pages/Auth";
+import Account from "./pages/Account";
+import Navbar from "./components/layout/Navbar";
+
+
+function App() {
+  return(
+    <BrowserRouter>
+    <div className = "min-h-screen flex flex-col">
+      <Navbar /> 
+      <main className = "flex-1">
+          <Routes>
+            <Route index element = {<Home />}/>
+            <Route path="/onboarding" element = {<Onboarding />}/>
+            <Route path="/profile" element = {<Profile />}/>
+            <Route path="/auth/:pathname" element = {<Auth />}/>
+            <Route path="/account/:pathname" element = {<Account />}/>
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
